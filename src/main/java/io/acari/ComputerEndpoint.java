82bd98f5b3462c512ae8e4ca8e8a8e3687d3f7eb
cyclic-reference <alex@cyclicreference.space>
2017-04-27 15:57:10 -0500

    Added Endpoint

---
+++ src/main/java/io/acari/ComputerEndpoint.java
@@ -0,0 +1,28 @@
+package io.acari;
+
+import io.acari.simple.web_service.GetAllComputerRequest;
+import io.acari.simple.web_service.GetAllComputerResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class ComputerEndpoint {
+    private static final String SERVICE_ENDPOINT = "http://acari.io/simple/web-service";
+    private ComputerRepository computerRepository;
+
+    @Autowired
+    public ComputerEndpoint(ComputerRepository computerRepository) {
+        this.computerRepository = computerRepository;
+    }
+
+    @ResponsePayload
+    @PayloadRoot(namespace = SERVICE_ENDPOINT, localPart = "getAllComputerRequest")
+    public GetAllComputerResponse getAllComputerResponse(@RequestPayload GetAllComputerRequest getAllComputerRequest){
+        GetAllComputerResponse getAllComputerResponse = new GetAllComputerResponse();
+        getAllComputerResponse.getComputer().addAll(computerRepository.getAllComputers());
+        return getAllComputerResponse;
+    }
+}
