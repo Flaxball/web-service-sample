ba5d724ea21ce6802b86eadb8da3fe37596e1fc6
cyclic-reference <alex@cyclicreference.space>
2017-04-26 19:25:41 -0500

    Intial Commit

---
+++ src/main/java/io/acari/WebServiceSampleApplication.java
@@ -0,0 +1,12 @@
+package io.acari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceSampleApplication.class, args);
+	}
+}
