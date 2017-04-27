b5b1cc24b75a91e97e1efb2301735509ff90d1fd
cyclic-reference <alex@cyclicreference.space>
2017-04-27 15:42:34 -0500

    Update Schema, Created Repo

---
+++ src/main/java/io/acari/ComputerRepository.java
@@ -0,0 +1,43 @@
+package io.acari;
+
+import io.acari.simple.web_service.Computer;
+import io.acari.simple.web_service.Cores;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class ComputerRepository {
+    private static final Map<String, Computer> modelToComputer = new HashMap<>();
+
+
+    @PostConstruct
+    public void setUp(){
+        Computer sandwich = buildComputer(Cores.TWO, 16, "Razer", "Blade Stealth");
+        modelToComputer.put(sandwich.getModel(), sandwich);
+        Computer dellXps13 = buildComputer(Cores.TWO, 8, "Dell", "XPS 13");
+        modelToComputer.put(dellXps13.getModel(),dellXps13);
+        Computer bladePro = buildComputer(Cores.FOUR, 16, "Razer", "Blade Pro");
+        modelToComputer.put(bladePro.getModel(),bladePro);
+        Computer macPro = buildComputer(Cores.EIGHT, 16, "Apple", "Macbook Pro");
+        modelToComputer.put(macPro.getModel(),macPro);
+    }
+
+
+    public Collection<Computer> getAllComputers(){
+        return modelToComputer.values();
+    }
+
+    private Computer buildComputer(Cores cores, int ram, String make, String model){
+        Computer computer = new Computer();
+        computer.setMake(model);
+        computer.setModel(make);
+        computer.setRam(ram);
+        computer.setCores(cores);
+        return computer;
+    }
+}
